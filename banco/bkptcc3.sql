6eb3ef690d185585b22b4d302bf9923aa3b050ba
Melissa Vieira dos Santos <melissacgv1@gmail.com>
2026-08-04 14:13:02 -0300

    Update bkptcc3.sql

--- banco/bkptcc3.sql
+++ banco/bkptcc3.sql
@@ -30,7 +30,21 @@ CREATE TABLE IF NOT EXISTS `aulas` (
   PRIMARY KEY (`id_aula`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela stardev.aulas: ~12 rows (aproximadamente)
+DELETE FROM `aulas`;
+INSERT INTO `aulas` (`id_aula`, `materia`, `duracao`, `qtd_aulas`) VALUES
+	(1, 'Back-End', '1hora', 12),
+	(2, 'FundamentosProgramacao', '1hora', 10),
+	(3, 'LogicaProgramacao', '1hora', 10),
+	(4, 'DesenvolvimentoWeb', '1hora', 11),
+	(5, 'Front-End', '1hora', 11),
+	(6, 'BancoDados', '1hora', 10),
+	(7, 'ProjetoSoftware', '1hora', 12),
+	(8, 'SegurancaInformacao', '1hora', 12),
+	(9, 'CodigoLimpo', '1hora', 12),
+	(10, 'DesenvolvimentoMobile', '1hora', 13),
+	(11, 'RedesIOT', '1hora', 10),
+	(12, 'LinguagemProgramacao', '1hora', 12);
 
 -- Copiando estrutura para tabela stardev.cadastro
 DROP TABLE IF EXISTS `cadastro`;
@@ -42,9 +56,13 @@ CREATE TABLE IF NOT EXISTS `cadastro` (
   `telefone` varchar(15) NOT NULL DEFAULT '',
   `nivel` varchar(50) DEFAULT 'U',
   PRIMARY KEY (`id_cadastro`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela stardev.cadastro: ~2 rows (aproximadamente)
+DELETE FROM `cadastro`;
+INSERT INTO `cadastro` (`id_cadastro`, `nome`, `email`, `senha`, `telefone`, `nivel`) VALUES
+	(15, 'Lauane Gazola', 'lauanegazola@gmail.com', '$2b$10$N7C1CdfbwVM1ve9V7zO7ke7.UPLHOAlOlUxrgm5icwSCRNRNi4W.G', '(18) 99653-4326', 'U'),
+	(16, 'STARDEV', 'stardevaulas@gmail.com', '$2b$10$9nhJ99/wWi4EJIh4uXINie54Fhomuu2twV1zm9Jre.MQOV6tr0D.e', 'stardevaulas@gm', 'A');
 
 -- Copiando estrutura para tabela stardev.contato
 DROP TABLE IF EXISTS `contato`;
@@ -56,9 +74,28 @@ CREATE TABLE IF NOT EXISTS `contato` (
   `data_envio` timestamp NOT NULL DEFAULT current_timestamp(),
   `status_feedback` varchar(20) DEFAULT 'Pendente',
   PRIMARY KEY (`id_contato`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela stardev.contato: ~17 rows (aproximadamente)
+DELETE FROM `contato`;
+INSERT INTO `contato` (`id_contato`, `nome`, `email`, `comentario`, `data_envio`, `status_feedback`) VALUES
+	(1, 'back', 'stardevaulas@gmail.com', 'shfghfg', '2026-05-05 13:24:30', 'Pendente'),
+	(2, 'melissa cristina', 'melissasteste@hotmail.cm', 'qqqqqqqqqqqqqqqqqqq', '2026-05-05 13:24:30', 'Pendente'),
+	(3, 'melissa cristina', 'melissasteste@hotmail.cm', 'rrrrrrrrrrrrrrrrr', '2026-05-05 13:24:30', 'Pendente'),
+	(4, 'Teste Nome', 'teste@email.com', 'Isso é um comentário válido', '2026-05-05 13:24:30', 'Pendente'),
+	(5, 'Melissa Vieira', 'melissacgv1@gmail.com', 'kkkkkkkkkkkkkkkkkkkkkkkkkk', '2026-05-05 13:24:30', 'Pendente'),
+	(6, 'testando se fun', 'testetestete@gmail.com', 'kkkkkkkkkkkkkkkkkkkkkkkkkk', '2026-05-05 13:24:30', 'Pendente'),
+	(7, 'BRUNO FERREIRA', 'BRUNÃOFERREIRÃO', 'TESTANDO O CONTATO', '2026-05-05 13:24:30', 'Pendente'),
+	(8, 'Maria Laura Pasquini Ribeiro', 'marialaurapasquini@gmail.com', 'Olá sou irmã da Lauane', '2026-05-05 13:24:30', 'Pendente'),
+	(9, 'Lauane Pasquini Ribeiro', 'lauane.ribeiro@aluno.senai.br', 'Melhor site que já vi em minha vida <3', '2026-05-05 13:24:30', 'Pendente'),
+	(10, 'Lauane Pasquini', 'lauanepasquini@email.com', 'Melhor site do mundo ', '2026-05-05 13:24:30', 'Pendente'),
+	(11, 'Lauane Pasquini', 'lauanepasquini@email.com', 'Melhor site', '2026-05-05 13:24:30', 'Pendente'),
+	(12, 'Lauane Pasquini Ribeiro', 'lauanepasquini@email.br', 'Melhor site do mundo', '2026-05-05 13:24:30', 'Pendente'),
+	(13, 'Lauane Pasquini', 'lauane@email.com', 'lauane megamente', '2026-05-05 13:24:30', 'Pendente'),
+	(14, 'Kemilly Reginão', 'reginao@email.com', 'Reginão gostou', '2026-05-05 13:24:30', 'Pendente'),
+	(15, 'Kemilly Reginão', 'reginao@email.com', 'Reginão gostou', '2026-05-05 13:24:30', 'Pendente'),
+	(17, 'Filipe Deshasp', 'feliperer@email.com', 'ajfhasjkdfhajksdfhkjsdf', '2026-05-05 13:24:30', 'Pendente'),
+	(18, 'Érick Jacquin', 'jacquin@email.com', 'trouxe bolinho de chocolate para o Brasil!', '2026-05-19 19:35:13', 'Pendente');
 
 -- Copiando estrutura para tabela stardev.materias
 DROP TABLE IF EXISTS `materias`;
@@ -71,9 +108,12 @@ CREATE TABLE IF NOT EXISTS `materias` (
   PRIMARY KEY (`id_materias`),
   KEY `id_aula` (`id_aula`),
   CONSTRAINT `id_aula` FOREIGN KEY (`id_aula`) REFERENCES `aulas` (`id_aula`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela stardev.materias: ~1 rows (aproximadamente)
+DELETE FROM `materias`;
+INSERT INTO `materias` (`id_materias`, `nome_aulas`, `id_aula`, `link`, `descricao`) VALUES
+	(1, 'CORES  editar', 1, 'https://www.youtube.com/watch?v=YIvGf2IjaXI', 'Testando o editar do vídeo.');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
